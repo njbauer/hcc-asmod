c55fd14ec45750a7a39d084b536de6f9c5d48b31
Nolan <agron_man@live.com>
2021-06-14 10:59:51 -0500

    Intial plug-in creation file

---
+++ hcc-asmod.php
@@ -0,0 +1,9 @@
+/*
+Plugin Name: hcc-asmod
+Plugin URI:  http://link to your plugin homepage
+Description: Modifies the Advanced Sermons Plug-in
+Version:     1.0
+Author:      Hopy Community Church
+Author URI:  https://hopecc.com
+License:     GNU General Public License v.3
+*/
